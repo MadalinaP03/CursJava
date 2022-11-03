b447474bc0a7c097889b30ae987ff9c748244fc4
MadalinaP03 <plotean.mady@gmail.com>
2022-11-03 17:56:12 +0200

    second commit

--- src/main/java/io/darkwings/Main.java
+++ src/main/java/io/darkwings/Main.java
@@ -1,7 +1,5 @@
 package io.darkwings;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Madalina");}
     }
-}
